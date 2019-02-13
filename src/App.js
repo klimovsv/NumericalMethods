9d2b217b87baa24a3cbda50e781f526857b2d9d4
Klimov Sergey <sergey.klimov.97@yandex.ru>
2019-02-13 14:00:42 +0300

    add graphics output

--- src/App.js
+++ src/App.js
@@ -204,21 +204,35 @@ class App extends Component {
                         const diff = data.diff;
                         const user = data.user;
                         const res = data.result;
+                        const number = data.data.number;
                         const conc = this.concat([diff,user,res]);
-                        // console.log(conc)
-                        const zipped = this.zip(time,conc,["dx","dy","x","y","xr","yr"]);
+                        let names = [["dx","x_user","x_computed"],["dy","y_user","y_computed"],["dz","z_user","z_computed"]];
+                        names = names.slice(0,number);
+                        let new_names = [];
+                        for(let i = 0 ; i < 3; i++){
+                            names.forEach((name_arr) =>{
+                                new_names.push(name_arr[i])
+                            })
+                        }
+                        const zipped = this.zip(time,conc,new_names);
                         return (
-                            <LineChart width={730} height={250} data={zipped}
-                                       margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="time" />
-                                <YAxis />
-                                <Tooltip />
-                                <Legend />
-                                <Line type="linear" dot={false} dataKey="x" stroke="#8884d8" />
-                                <Line type="linear" dot={false} dataKey="xr" stroke="#82ca9d" />
-                                <Line type="linear" dot={false} dataKey="dx" stroke="#82ca9d" />
-                            </LineChart>
+                            names.map((name_arr) => {
+                                return (
+                                    <div className="App">
+                                        <LineChart width={730} height={250} data={zipped}
+                                                   margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                                            <CartesianGrid strokeDasharray="3 3" />
+                                            <XAxis dataKey="time" />
+                                            <YAxis />
+                                            <Tooltip />
+                                            <Legend />
+                                            <Line type="linear" dot={false} dataKey={name_arr[0]} stroke="#8884d8" />
+                                            <Line type="linear" dot={false} dataKey={name_arr[1]} stroke="#82ca9d" />
+                                            <Line type="linear" dot={false} dataKey={name_arr[2]} stroke="#82ca9d" />
+                                        </LineChart>
+                                    </div>
+                                )
+                            })
                         )
                     })() : null}
                 </div>
